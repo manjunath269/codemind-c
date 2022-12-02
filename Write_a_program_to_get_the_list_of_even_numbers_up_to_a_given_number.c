1556106471ca0016c053d608179357cc40273af7
manjunath269 <119472017+manjunath269@users.noreply.github.com>
2022-12-02 11:17:41 +0530

    #include<stdio.h> int main() {     int m ,n;     scanf("%d%d",&m,&n);     for(int i=m;i<=n;i++)     {         if(i%2==0)         printf("%d ",i);     } }

--- Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
+++ Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
 int main()
 {
-   int m ,n ,i;
-   scanf("%d%d",&m,&n);
-   for(i=m;i<=n;i++)
-   
-   {
-       if(i%2==0)
-       {
-   printf("%d",i);
-   printf(" ");
-       }
-   }
+    int m ,n;
+    scanf("%d%d",&m,&n);
+    for(int i=m;i<=n;i++)
+    {
+        if(i%2==0)
+        printf("%d ",i);
+    }
 }
