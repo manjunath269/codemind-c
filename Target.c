002fb528464380aab8d78c062b50a9117463a55d
manjunath269 <119472017+manjunath269@users.noreply.github.com>
2022-12-11 19:47:45 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d%d%d%d",&n,&i,&j,&k);     if(n>=10 && i>=10 && j>=10 && k>=10)     printf("YES");     else     printf("NO"); }

---
+++ Target.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d%d%d%d",&n,&i,&j,&k);
+    if(n>=10 && i>=10 && j>=10 && k>=10)
+    printf("YES");
+    else
+    printf("NO");
+}
+    
