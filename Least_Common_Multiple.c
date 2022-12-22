80b8f1af4407624be6a5fa65929fe9003aa9c503
manjunath269 <119472017+manjunath269@users.noreply.github.com>
2022-12-22 14:42:46 +0530

    #include<stdio.h> int main() {     int a,b,i=1,m;     scanf("%d%d",&a,&b);     while(1)     {         m=a*i;         if(m%b==0)         {             printf("%d",m);             break;         }         i++;     }
    }

---
+++ Least_Common_Multiple.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i=1,m;
+    scanf("%d%d",&a,&b);
+    while(1)
+    {
+        m=a*i;
+        if(m%b==0)
+        {
+            printf("%d",m);
+            break;
+        }
+        i++;
+    }
+    
+}
+    
